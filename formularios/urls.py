2daf9f0c8271ab289c103e970917b27df29cf62b
Henrique Dourado Domingos <125815196+henriquedouradoo@users.noreply.github.com>
2023-08-06 01:19:46 -0300

    Delete formularios directory

--- formularios/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('processa_formulario/', views.processa_formulario, name="processa_formulario")
-]
